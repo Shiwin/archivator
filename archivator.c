5ef70549da487666e2d5e23620e9050f1914056d
Ivan Sharavuev <shpiwan@gmail.com>
2019-03-05 20:37:33 +0300

    Implement 2 & 3 argument usage. Refactore code.

--- archivator.c
+++ archivator.c
@@ -33,6 +33,10 @@ struct file_meta_info
 };
 #pragma pack(pop)
 
+int archive_dirent(const struct dirent *c_d,
+                   const int fd, const char *relative_path);
+int archive_folder(const char *dirname, const int archive_fd);
+
 /*
 d_name == "." or ".." => 0 (false)
 else => 1 (true)
@@ -41,6 +45,7 @@ int validate_name(const char *d_name)
 {
     return strcmp(".", d_name) != 0 && strcmp("..", d_name) != 0;
 }
+
 /*
     Just print the name of file which is represented
     by the input parameter
@@ -64,16 +69,47 @@ int dump_dirent(const struct dirent *current_dirent, const int fd, const char *r
     return result;
 }
 
+int dirent_handler_metafunction(dirent_handler dh, const char *dirname, const int archive_fd)
+{
+
+    DIR *dir;
+    if ((dir = opendir(dirname)) == NULL)
+    {
+        printf("Cannot open directory %s\n", dirname);
+        return -1;
+    }
+
+    struct dirent *current_dirent;
+
+    // iter over files
+    while ((current_dirent = readdir(dir)) != NULL)
+    {
+        dh(current_dirent, archive_fd, dirname);
+    }
+    return 0;
+}
+
+int archive_folder(const char *dirname, const int archive_fd)
+{
+    return dirent_handler_metafunction(archive_dirent, dirname, archive_fd);
+}
+
+int dump_folder(const char *dirname)
+{
+    return dirent_handler_metafunction(dump_dirent, dirname, 0);
+}
+
 /*
 Return values:
 -2 - Critical: Error writing to the archive. May be stop program with error status(???)
 1 - OK
 0 - current file was not written to an archive
 */
-int archive_dirent(const struct dirent *current_dirent, const int fd, const char *relative_path)
+int archive_dirent(const struct dirent *c_d,
+                   const int fd, const char *relative_path)
 {
     int result = 1;
-    const char *d_name = current_dirent->d_name;
+    const char *d_name = c_d->d_name;
     if (fd < 0)
     {
         printf("Error: can't write to archive\n");
@@ -84,8 +120,23 @@ int archive_dirent(const struct dirent *current_dirent, const int fd, const char
     {
         if (validate_name(d_name))
         {
+            // construct relative path to file
+            char d_name_path[MAX_FILENAME_LENGTH];
+            strcat(d_name_path, relative_path);
+            strcat(d_name_path, "/");
+            strcat(d_name_path, d_name);
+
             struct stat current_stat;
-            if (stat(d_name, &current_stat))
+            int read_fd = open(d_name_path, O_RDONLY);
+
+            if (read_fd < 0)
+            {
+                printf("Can't open file for reading: %s\n", d_name_path);
+                result = !result;
+                goto exit;
+            }
+
+            if (fstat(read_fd, &current_stat) != 0)
             {
                 printf("Can't get stat for the file: %s. "
                        "Igrone it.\n",
@@ -95,7 +146,7 @@ int archive_dirent(const struct dirent *current_dirent, const int fd, const char
             }
             if (S_ISREG(current_stat.st_mode))
             {
-                printf("%s is a regular file\n", d_name);
+                printf("%s is a regular file\n", d_name_path);
                 struct file_meta_info m_i =
                     {
                         .type = REGULAR_FILE,
@@ -106,12 +157,6 @@ int archive_dirent(const struct dirent *current_dirent, const int fd, const char
 
                 ssize_t written_bytes = write(fd, &m_i, sizeof(m_i));
                 char temp_buff[TEMP_BUFFER_SIZE];
-                int read_fd = open(d_name, O_RDONLY);
-                if (read_fd < 0)
-                {
-                    printf("Can't open file for reading: %s\n", d_name);
-                    result = !result;
-                }
                 long total_written_file_bytes = 0;
                 ssize_t read_bytes = read(read_fd, temp_buff, TEMP_BUFFER_SIZE);
                 while (read_bytes > 0)
@@ -140,20 +185,22 @@ int archive_dirent(const struct dirent *current_dirent, const int fd, const char
                     printf("Error during writing %s to the archive\n", d_name);
                     result = -2;
                 }
-            close_reading_file:
-                if (close(read_fd))
-                {
-                    printf("Closing file error: %s\n", d_name);
-                }
-                goto exit;
+                goto close_reading_file;
             }
             if (S_ISDIR(current_stat.st_mode))
             {
-                printf("%s is a directory\n", d_name);
+                printf("%s is a directory\n", d_name_path);
+                archive_folder(d_name_path, fd);
                 goto exit;
             }
             printf("Unsupported file type of file: name=%s\n", d_name);
             result = !result;
+        close_reading_file:
+            if (close(read_fd))
+            {
+                printf("Closing file error: %s\n", d_name_path);
+            }
+            goto exit;
         }
     }
 exit:
@@ -164,40 +211,36 @@ int main(int argc, char const *argv[])
 {
     if (!(argc == 2 || argc == 3))
     {
-        printf("Invalid number of arguments: %d\n", argc);
+        printf("Invalid number of arguments: %d\n"
+               "Usage example: ./archivator <dir> <archive_name>",
+               argc);
         return -1;
     }
 
     const char *dirname = argv[1];
+    const char *archive_name = argv[2];
 
-    DIR *dir;
-    if ((dir = opendir(dirname)) == NULL)
+    if (argc == 2)
     {
-        printf("Cannot open directory %s\n", dirname);
-        return -1;
+        dump_folder(dirname);
     }
 
-    dirent_handler *dh = archive_dirent;
-    int fd = open(dirname,
-                  O_WRONLY | O_CREAT | O_EXCL, ARCHIVE_PERMISSIONS);
-    if (fd < 0)
+    if (argc == 3)
     {
-        printf("Can't create archive file: %s\n", strerror(errno));
-        return -1;
+        int fd = open(archive_name,
+                      O_WRONLY | O_CREAT | O_EXCL, ARCHIVE_PERMISSIONS);
+        if (fd < 0)
+        {
+            printf("Can't create archive file: %s\n", strerror(errno));
+            return -1;
+        }
+        archive_folder(dirname, fd);
+        if (close(fd))
+        {
+            printf("Error during writing an archive."
+                   "Can't close an archive file\n");
+        }
     }
 
-    struct dirent *current_dirent;
-
-    // iter over files
-    while ((current_dirent = readdir(dir)) != NULL)
-    {
-        dh(current_dirent, fd, dirname);
-    }
-
-    if (close(fd))
-    {
-        printf("Error during writing an archive."
-               "Can't close an archive file\n");
-    }
     return 0;
 }
